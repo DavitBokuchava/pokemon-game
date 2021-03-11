a2b72bda956bb3182c3ffc2b97cfd55d979dd618
DavitBokuchava <davitbokuchava@gmail.com>
2021-03-11 04:40:23 +0400

    Homework-2
    i hope it 's well done!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import logo from './logo.svg';
+import bg2 from './assets/bg2.jpg';
+import bg3 from './assets/bg3.jpg';
+import Header from './components/header/Header';
+import Layout from './components/layout/Layout';
+import Footer from './components/footer/Footer';
+import './index.css';
+//import './App.css';
 
 function App() {
+  //const [urlBg, setUrlBg] = React.useState({})
+  const title = 'This is title';
+  const descr ='This is Description!'; 
+  const colorBg = '#99FFFF';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.js!
-        </p>
-       
-      </header>
+       <Header title = { title } descr = { descr } />
+        <Layout 
+          title = { title } 
+          descr = { descr } 
+          urlBg = {bg2}  />
+          <Layout 
+          title = { title } 
+          descr = { descr }
+          colorBg = { colorBg }  />
+          <Layout 
+          title = { title } 
+          descr = { descr }
+          urlBg = {bg3}
+          colorBg = { colorBg }  />
+       <Footer/>
+      
     </div>
   );
 }
