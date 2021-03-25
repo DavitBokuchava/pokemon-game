a08b10521fd197cb3875145c42e248ec3f2d9655
DavitBokuchava <davitbokuchava@gmail.com>
2021-03-25 21:15:49 +0400

    homework-7 commit6
    commit6

--- src/routes/GamePage/routes/Board/index.js
+++ src/routes/GamePage/routes/Board/index.js
@@ -1,5 +1,6 @@
 import {useContext} from 'react';
 import { PokemonContext } from '../../../../context/pokemonContext';
+import PokemonCard from '../../../../components/PockemonCard';
 import s from './style.module.css';
 
 const BoardPage = () => {
@@ -8,9 +9,22 @@ const BoardPage = () => {
 
     return (
         <div className={s.root}>
-						<div className={s.playerOne}>
-
-						</div>
+		    <div className={s.playerOne}>
+            {
+                    Object.entries(contText.pokemons).map(([key,{name,img,id,type,values}])=>
+                        < PokemonCard 
+                        className = {s.card}
+                        key = {key}
+                        name = {name}
+                        img = {img}
+                        id = {id} 
+                        type = {type} 
+                        values =  {values}
+                        isActive = {true}
+                        minimize = {true}
+                         />)
+                    }
+            </div>
             <div className={s.board}>
                 <div className={s.boardPlate}>1</div>
                 <div className={s.boardPlate}>2</div>
